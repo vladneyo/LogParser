0db4c7cab031576a3129b212be228167220bfeda
vneych <vneych@softserveinc.com>
2018-01-25 01:49:09 +0200

    AccessLogRepository was created.

--- LogParser.Data.Contracts/IAccessLogRepository.cs
+++ LogParser.Data.Contracts/IAccessLogRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace LogParser.Data.Contracts
+﻿using System;
+using System.Collections.Generic;
+using LogParser.EDM.Models;
+
+namespace LogParser.Data.Contracts
 {
     public interface IAccessLogRepository
     {
+        List<AccessLog> GetAll();
+
+        List<AccessLog> GetAll(int offset, DateTime? start = null, DateTime? end = null, int limit = 10);
+
+        List<string> GetTopHosts(int amount = 10, DateTime? start = null, DateTime? end = null, bool decs = false);
+
+        List<string> GetTopRoutes(int amount = 10, DateTime? start = null, DateTime? end = null, bool decs = false);
+
+        List<AccessLog> CreateBulk(List<AccessLog> list);
     }
 }
---
+++ LogParser.Data/Repositories/AccessLogRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LogParser.Data.Contracts;
+using LogParser.EDM;
+using LogParser.EDM.Models;
+
+namespace LogParser.Data.Repositories
+{
+    public class AccessLogRepository : IAccessLogRepository
+    {
+        public List<AccessLog> CreateBulk(List<AccessLog> list)
+        {
+            using (ParserContext ctx = new ParserContext())
+            {
+                ctx.AccessLogs.AddRange(list);
+                ctx.SaveChanges();
+                return list;
+            }
+        }
+
+        public List<AccessLog> GetAll()
+        {
+            using (ParserContext ctx = new ParserContext())
+            {
+                return ctx.AccessLogs.ToList();
+            }
+        }
+
+        public List<AccessLog> GetAll(int offset, DateTime? start = null, DateTime? end = null, int limit = 10)
+        {
+            using (ParserContext ctx = new ParserContext())
+            {
+                var query = ctx.AccessLogs
+                    .Select(x => x)
+                    .OrderBy(x => x.Time)
+                    .AsQueryable();
+
+                query = FilterByDates(query, start, end);
+
+                return query
+                    .Skip(offset)
+                    .Take(limit)
+                    .ToList();
+            }
+        }
+
+        public List<string> GetTopHosts(int amount = 10, DateTime? start = null, DateTime? end = null, bool decs = false)
+        {
+            return GetTop(x => x.Host, amount, start, end, decs);
+        }
+
+        public List<string> GetTopRoutes(int amount = 10, DateTime? start = null, DateTime? end = null, bool decs = false)
+        {
+            return GetTop(x => x.Route, amount, start, end, decs);
+        }
+
+        private IQueryable<AccessLog> FilterByDates(IQueryable<AccessLog> query, DateTime? start, DateTime? end)
+        {
+            if (start != null)
+            {
+                query = query.Where(x => x.Time >= start);
+            }
+            if (end != null)
+            {
+                query = query.Where(x => x.Time <= end);
+            }
+            return query;
+        }
+
+        private List<string> GetTop(Func<AccessLog, string> exp, int amount = 10, DateTime? start = null, DateTime? end = null, bool decs = false)
+        {
+            using (ParserContext ctx = new ParserContext())
+            {
+                var query = ctx.AccessLogs
+                    .Select(x => x);
+
+                query = decs ?
+                    query.OrderByDescending(x => x.Time).AsQueryable() :
+                    query.OrderBy(x => x.Time).AsQueryable();
+
+                query = FilterByDates(query, start, end);
+
+                return query
+                    .Select(exp)
+                    .Take(amount)
+                    .ToList();
+            }
+        }
+    }
+}
