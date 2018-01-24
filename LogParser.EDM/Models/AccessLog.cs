ea60a588677cb7a9121880cf438256e876c2357a
vneych <vneych@softserveinc.com>
2018-01-25 00:25:49 +0200

    DbContext and AccessLog model were added.

---
+++ LogParser.EDM/Models/AccessLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LogParser.EDM.Models
+{
+    public class AccessLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        [Index(IsClustered = false, IsUnique = false)]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public string Route { get; set; }
+
+        public string QueryParams { get; set; }
+
+        [Required]
+        public int RequestStatus { get; set; }
+
+        [Required]
+        public int ResponseSize { get; set; }
+
+        public string Geolocation { get; set; }
+    }
+}
---
+++ LogParser.EDM/ParserContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using LogParser.EDM.Models;
+
+namespace LogParser.EDM
+{
+    public class ParserContext : DbContext
+    {
+        public ParserContext() : base("ParserDbConnection")
+        {
+            
+        }
+
+        public DbSet<AccessLog> AccessLogs { get; set; }
+    }
+}
